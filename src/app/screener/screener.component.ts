8af19aeaf0f269e890ba29dfccf1b07897bf10d2
sjoseph1848 <sjoseph1848@gmail.com>
2020-08-18 23:06:51 -0500

    added the stock screening form still in progress

--- src/app/screener/screener.component.ts
+++ src/app/screener/screener.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-screener',
@@ -14,13 +15,32 @@ export class ScreenerComponent implements OnInit {
   defaultSector = "Technology";
   defaultIndustry = "Software";
   defaultExchange = "nyse";
-  constructor() { }
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
     console.log(form);
+    if (form.valid) {
+      let screenedStock = this.getScreenedStocks(form.value);
+      screenedStock.subscribe(data => {
+        console.log(data);
+      })
+    }
+  }
+
+  getScreenedStocks(screenData: any) {
+    const data = screenData.screenData;
+    console.log(data)
+    let url = `https://nottingham-1848-slayer-robin.azurewebsites.net/api/screen/marketCap/${data.marketCap}/beta/${data.betaMin}/volumeMin/${data.volumeMin}/sector/${data.sector}/industry/None/exchange/${data.exchange}/dividendMin/${data.dividendMin}`;
+    console.log(url);
+    return this.http.get(url);
   }
 
 }
+
+
+
